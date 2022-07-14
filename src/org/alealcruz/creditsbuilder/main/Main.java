cb53d2e362aea032c98a095f14b18870b1854845
alealcruz <alealcruz@gmail.com>
2022-07-14 17:29:33 +0200

    Fixed the behaviour of invocation arguments
    Fixed the behaviour of invocation arguments (their value was not taken into consideration).

--- src/org/alealcruz/creditsbuilder/main/Main.java
+++ src/org/alealcruz/creditsbuilder/main/Main.java
@@ -4,8 +4,10 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.alealcruz.creditsbuilder.misc.Disclaimer;
@@ -19,31 +21,35 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Set<String> set = new HashSet<>(Arrays.asList(args));
+		Map<String,String> map = new HashMap<String,String>();
 		String xmlFromInvokation = "contentv3.xml";
 		String outputFileName = "credits";
 		String imgPattern = "IMG_MAT6PRI_";
 		String vidPattern = "VID_MAT6PRI_";
-		String reaPattern = "REA03_";
+		String reaPattern = "REA04_";
 		
 		System.out.println(Disclaimer.getDisclaimerText());
 		
-		Instructions.invokation(set, xmlFromInvokation, outputFileName, imgPattern, vidPattern, reaPattern, args);
+		Instructions.invokation(set, map,xmlFromInvokation, outputFileName, imgPattern, vidPattern, reaPattern, args);
 		
-		String content = Generation.getFileContent(xmlFromInvokation);
+		System.out.println("Current working configuration:\n"+map.values());
+		
+		
+		String content = Generation.getFileContent(map.get("xml"));//xmlFromInvokation);
 		List<String> pages = Generation.getPages(content, "<instance class=\"exe.engine.node.Node\" reference=");
 		Set<Credit> IMG_listWithoutRepetitions = new HashSet<Credit>(), VID_listWithoutRepetitions = new HashSet<Credit>();
 		int pageNumber = 0;
 		for (String page : pages) {
 			pageNumber++;
 			try {
-				Generation.findInstancesOnlyImages("&lt;img", "&gt;", imgPattern, reaPattern, page, IMG_listWithoutRepetitions,
+				Generation.findInstancesOnlyImages("&lt;img", "&gt;", map.get("img")/*imgPattern*/, map.get("rea")/*reaPattern*/, page, IMG_listWithoutRepetitions,
 						pageNumber,true);
 			} catch (Exception e) {
 				System.out.println("Exception while processing image files\n");
 			}
 			
 			try {
-				Generation.findInstancesOnlyVideos("&lt;video", "/video&gt;", vidPattern, reaPattern, page,
+				Generation.findInstancesOnlyVideos("&lt;video", "/video&gt;", map.get("vid")/*vidPattern*/, map.get("rea")/*reaPattern*/, page,
 						VID_listWithoutRepetitions, pageNumber);
 			} catch (Exception e) {
 				System.out.println("Exception while processing video files\n");
@@ -58,7 +64,7 @@ public class Main {
 		
 		System.out.println("\nIt was found IMAGE and/or VIDEO " + finalList.size() + " elements along " + Generation.differentPages(finalList) + " different pages");
 		List<List<Credit>> listOfLists = Generation.doListOfLists(finalList);
-		Generation.writeCreditsInOutputFile(listOfLists, outputFileName + "_" + new Timestamp((new Date()).getTime()) + ".html");
+		Generation.writeCreditsInOutputFile(listOfLists, map.get("output")/*outputFileName*/ + "_" + new Timestamp((new Date()).getTime()) + ".html");
 		
 		FinalInformation.showAsciiArt();
 		
